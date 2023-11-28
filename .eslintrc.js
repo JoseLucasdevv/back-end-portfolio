047ae8cb7d75515cc15538d13b58fc9850dcc216
JoseLucasdevv <joselucas.dev@gmail.com>
2023-11-28 08:49:14 -0300

    feat:add eslintrc config

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+
+  extends: "eslint:recommended",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+
+    sourceType: "module",
+  },
+  rule: {},
+};
