3f04f212b5df844fa0f031d7ea7fa2db4605b498
JoseLucasdevv <joselucas.dev@gmail.com>
2023-12-28 08:02:49 -0300

    feat:add WELCOME API end Point /

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,6 +3,38 @@ const { emailController } = require("../controller/sendEmailController");
 
 const routes = express.Router();
 
+routes.get("/", (req, res) => {
+  const htmlContent = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <style>
+        body {
+          font-family: 'Arial', sans-serif;
+          background-color: #f0f0f0;
+          text-align: center;
+          padding: 50px;
+        }
+        h1 {
+          color: #333;
+        }
+        p {
+          color: #666;
+        }
+      </style>
+      <title>Bem-vindo à API Send Email</title>
+    </head>
+    <body>
+      <h1>Bem-vindo à API Send Email</h1>
+      <p>Obrigado por se juntar a nós!</p>
+    </body>
+    </html>
+  `;
+  res.send(htmlContent);
+});
+
 routes.post("/email", emailController);
 
 routes.get("/email", emailController);
